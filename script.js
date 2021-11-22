7d56da100274114d16dd8b5c8545436bbad5ab12
joe byrne <joebyrne@joes-MacBook-Pro.local>
2021-11-22 16:55:44 +0000

    logic done

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+const days = document.getElementById('days-number');
 const hours = document.getElementById('hours-number');
 const minutes = document.getElementById('minutes-number');
 const seconds = document.getElementById('seconds-number');
 
-console.log('jpours', hours);
+const newYears = "1 Jan 2022";
 
 
 function everyTime() {
+    const newYearsDate = new Date(newYears);
     const tDate = new Date;
 
+    const totalSeconds = (newYearsDate - tDate) / 1000;
 
+    let daysTime = Math.floor(totalSeconds / 3600 / 24)
     let hoursTime = tDate.getHours();
     let minutesTime = tDate.getMinutes();
     let secondsTime = tDate.getSeconds();
 
+    days.innerHTML = daysTime;
     hours.innerHTML = 24 - hoursTime;
     minutes.innerHTML = 60 - minutesTime;
     seconds.innerHTML = 60 - secondsTime;  
