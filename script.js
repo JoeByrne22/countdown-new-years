6046fd0b4ad52af53599bf58abde7af80d3e0952
joe byrne <joebyrne@joes-MacBook-Pro.local>
2021-11-22 16:47:30 +0000

    hms done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('running');
+const hours = document.getElementById('hours-number');
+const minutes = document.getElementById('minutes-number');
+const seconds = document.getElementById('seconds-number');
+
+console.log('jpours', hours);
+
+
+function everyTime() {
+    const tDate = new Date;
+
+
+    let hoursTime = tDate.getHours();
+    let minutesTime = tDate.getMinutes();
+    let secondsTime = tDate.getSeconds();
+
+    hours.innerHTML = 24 - hoursTime;
+    minutes.innerHTML = 60 - minutesTime;
+    seconds.innerHTML = 60 - secondsTime;  
+}
+
+var myInterval = setInterval(everyTime, 1000);
